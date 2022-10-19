37e3d58813e35ccc32d3b4cbdc8a49393bd91b3e
ibal <muammadibal@gmail.com>
2022-10-19 15:35:32 +0700

    fixing enabled scroll

--- src/screen/home/index.js
+++ src/screen/home/index.js
@@ -1,35 +1,73 @@
-import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  ActivityIndicator,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 import {PanGestureHandler} from 'react-native-gesture-handler';
 import Animated, {
+  Extrapolate,
   interpolate,
+  scrollTo,
   useAnimatedGestureHandler,
+  useAnimatedRef,
   useAnimatedScrollHandler,
   useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
   withDelay,
+  withRepeat,
   withSpring,
 } from 'react-native-reanimated';
 import {runOnJS} from 'react-native-reanimated/lib/reanimated2/core';
-const END_POSITION = 200;
+const {width, height} = Dimensions.get('screen');
+let datas = [
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1661961112134-fbce0fdf3d99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1666090257483-32f8b567424b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1666058091341-42486f239ebc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1666065988253-3ad95ca4404f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1666094288098-c067a55c8e9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+  },
+  {
+    imageUrl:
+      'https://images.unsplash.com/photo-1664575600796-ffa828c5cb6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+  },
+];
+
 const Home = () => {
-  const refScroll = React.createRef();
+  const refScroll = useAnimatedRef();
   const scroll = useSharedValue(true);
   const position = useSharedValue(0);
   const [enabledScroll, setEnabledScroll] = useState(true);
-  const [layout, setLayout] = useState('list');
+  const [listData, setListData] = useState([]);
+  const [layoutHeight, setLayoutHeight] = useState(0);
+  const [loadMore, setLoadMore] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
 
-  useEffect(() => {
-    onFetch();
-  }, []);
-
-  const onFetch = async (page = 1, startFetch, abortFetch) => {
-    fetch(`https://reqres.in/api/users?page=${page}`)
+  const onFetch = async () => {
+    fetch(`${apiUrl}`)
       .then(res => res.json())
       .then(resVal => {
-        startFetch(resVal?.data, resVal?.per_page);
+        console.log('resVal', resVal);
+        // setListData(resVal?.data);
       })
       .catch(err => {
         abortFetch();
@@ -48,56 +86,124 @@ const Home = () => {
       ctx.startY = position.value;
     },
     onActive: (event, ctx) => {
-      if (event.translationY > 1 && event.translationY <= 150) {
-        runOnJS(setEnabledScroll)(false);
+      if (event.translationY > 1 && event.translationY <= 170) {
         position.value = ctx.startY + event.translationY;
       }
     },
     onEnd: (event, _) => {
-      if (event.translationY > 0) {
+      if (event.translationY > 100 && event.translationY < 130) {
+        position.value = withSpring(130);
         position.value = withDelay(3000, withSpring(0));
+        // withDelay(3000, runOnJS(setRepeatRotate)(false));
+      } else if (event.translationY > 130) {
+        position.value = withSpring(130);
+        position.value = withDelay(3000, withSpring(0));
+        // withDelay(3000, runOnJS(setRepeatRotate)(false));
+      } else {
+        position.value = withSpring(0);
       }
       runOnJS(setEnabledScroll)(true);
+      scrollTo(refScroll, 0, 1, true);
+    },
+    onCancel: (e, _) => {
+      scrollTo(refScroll, 0, 1, true);
     },
   });
 
-  const animatedStyle = useAnimatedStyle(() => {
+  useEffect(() => {
+    if (enabledScroll && layoutHeight <= height - 150) {
+      setEnabledScroll(false);
+    }
+  }, [enabledScroll]);
+
+  // useDerivedValue(() => {
+  //   if (enabledScroll) {
+  //     scrollTo(refScroll, 0, 1, true);
+  //     // refScroll?.current?.scrollTo({x: 0, y: 15});
+  //   }
+  // });
+
+  const animatedRefreshContainerStyle = useAnimatedStyle(() => {
+    const translateY = interpolate(position.value, [0, 150], [0, 70]);
     return {
-      transform: [{translateY: position.value}],
+      top: translateY,
+    };
+  });
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const translateY = interpolate(position.value, [0, 180], [0, 180], {
+      extrapolateRight: Extrapolate.CLAMP,
+    });
+    return {
+      transform: [{translateY}],
     };
   });
 
   const indicatorStyle = useAnimatedStyle(() => {
-    const scale = interpolate(position.value, [0, 80], [0, 1]);
-    const rotate = interpolate(position.value, [0, 80], [0, 360]);
-    const translateY = interpolate(position.value, [0, 80], [-100, -50]);
+    const scale = interpolate(position.value, [0, 80], [0, 1], {
+      extrapolateLeft: Extrapolate.CLAMP,
+      extrapolateRight: Extrapolate.EXTEND,
+    });
+    const rotate = interpolate(position.value, [0, 80], [0, 360], {
+      extrapolateRight: Extrapolate.EXTEND,
+    });
+    const translateY = interpolate(
+      position.value,
+      [0, 80],
+      [-100, -10],
+      Extrapolate.CLAMP,
+    );
     return {
-      transform: [{translateY}, {rotate: `${rotate}deg`}, {scale: scale}],
+      transform: [{translateY}, {rotate: `${rotate}deg`}, {scale}],
     };
   });
 
   const indicatorTextStyle = useAnimatedStyle(() => {
     const opacity = interpolate(position.value, [0, 80], [0, 1]);
-    const translateY = interpolate(position.value, [0, 80], [-100, 0]);
+    const scale = interpolate(position.value, [0, 80], [0, 0.7], {
+      extrapolateLeft: Extrapolate.CLAMP,
+      extrapolateRight: Extrapolate.EXTEND,
+    });
+    const translateY = interpolate(
+      position.value,
+      [0, 80],
+      [-100, 10],
+      Extrapolate.CLAMP,
+    );
     return {
-      // opacity: 10,
-      transform: [{translateY}],
+      opacity,
+      transform: [{translateY}, {scale}],
     };
   });
 
   return (
     <View style={{flex: 1}}>
+      <View
+        style={{
+          height: 60,
+          backgroundColor: 'white',
+          zIndex: 10,
+          justifyContent: 'center',
+          paddingHorizontal: 16,
+        }}>
+        <TouchableOpacity
+          onPress={() => {
+            refScroll?.current?.scrollToOffset({animated: true, offset: 1});
+          }}>
+          <Text style={{fontSize: 22, fontWeight: 'bold'}}>Instagram</Text>
+        </TouchableOpacity>
+      </View>
       <Animated.View
         style={[
+          animatedRefreshContainerStyle,
           {
-            height: 150,
-            backgroundColor: 'red',
+            backgroundColor: '#eaeaea',
             position: 'absolute',
-            top: 0,
             left: 0,
             right: 0,
             justifyContent: 'center',
             alignItems: 'center',
+            height: 150,
           },
         ]}>
         <Animated.View
@@ -107,10 +213,13 @@ const Home = () => {
               width: 30,
               height: 30,
               backgroundColor: 'green',
+              borderRadius: 30 / 3,
             },
           ]}
         />
-        <Animated.Text style={indicatorTextStyle}>Loading...</Animated.Text>
+        <Animated.Text style={[indicatorTextStyle, {color: 'black'}]}>
+          Loading...
+        </Animated.Text>
       </Animated.View>
       <PanGestureHandler
         enabled={!enabledScroll}
@@ -120,19 +229,57 @@ const Home = () => {
           style={[{flex: 1, backgroundColor: 'white'}, animatedStyle]}>
           <Animated.FlatList
             ref={refScroll}
-            onScrollBeginDrag={e => {
-              if (e.nativeEvent.contentOffset.y <= 0) {
+            onLayout={e => {
+              if (e.nativeEvent.layout.height <= height - 150) {
                 setEnabledScroll(false);
-              } else {
-                setEnabledScroll(true);
+                setLayoutHeight(e.nativeEvent.layout.height);
               }
+            }}
+            onScrollBeginDrag={e => {
+              if (e.nativeEvent.contentOffset.y > 0) {
+                return setEnabledScroll(true);
+              }
+
+              setEnabledScroll(false);
+            }}
+            onScroll={e => {
+              if (e.nativeEvent.contentOffset.y > 0) {
+                setLoadMore(true);
+                return setEnabledScroll(true);
+              }
+              setEnabledScroll(false);
             }}
             scrollEnabled={enabledScroll}
             scrollEventThrottle={16}
-            data={Array(100).fill('')}
+            data={datas} //Array(100).fill('')}
             renderItem={({item, index}) => {
-              return <Text>{index}</Text>;
+              return (
+                <View>
+                  <Image
+                    source={{uri: item?.imageUrl}}
+                    style={{width: width, height: 400}}
+                  />
+                  <Text>{index}</Text>
+                </View>
+              );
             }}
+            onEndReached={() => {
+              setTimeout(() => {
+                setLoadMore(false);
+              }, 3000);
+            }}
+            onEndReachedThreshold={0.1}
+            ListFooterComponent={
+              loadMore && (
+                <View
+                  style={{
+                    height: 50,
+                    justifyContent: 'center',
+                  }}>
+                  <ActivityIndicator size="large" />
+                </View>
+              )
+            }
           />
         </Animated.View>
       </PanGestureHandler>
